6c5c35cabb7961d9a1b031632ca91d94dafff9ae
Attila Buturla <AttilaButurla@gmail.com>
2019-08-24 14:04:15 -0700

    Basic styling

--- main.js
+++ main.js
@@ -14,7 +14,9 @@ function createWindow () {
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
-  })
+  });
+
+  mainWindow.maximize();
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
@@ -27,14 +29,14 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow = null;
   })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
